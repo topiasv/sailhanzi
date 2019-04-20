df344ee98fb0342f0ca1b79af1cc31a64651d99c
Topias Vainio <toxip@disroot.org>
2019-04-21 01:57:44 +0300

    Update translations

---
+++ translations/sailhanzi-zh-tw-old.ts
@@ -0,0 +1,66 @@
+<?xml version="1.0" encoding="utf-8"?>
+<!DOCTYPE TS>
+<TS version="2.1">
+<context>
+    <name>CoverPage</name>
+    <message>
+        <source>SailHanzi</source>
+        <translation>航海漢字</translation>
+    </message>
+</context>
+<context>
+    <name>MainPage</name>
+    <message>
+        <source>Convert</source>
+        <translation>轉換</translation>
+    </message>
+    <message>
+        <source>SailHanzi</source>
+        <translation>航海漢字</translation>
+    </message>
+    <message>
+        <source>Simplified</source>
+        <translation>簡體字</translation>
+    </message>
+    <message>
+        <source>Traditional</source>
+        <translation>繁體字</translation>
+    </message>
+    <message>
+        <source>Taiwanese</source>
+        <translation>台灣正體</translation>
+    </message>
+    <message>
+        <source>Hong Kong</source>
+        <translation>香港繁體</translation>
+    </message>
+    <message>
+        <source>Translate idioms</source>
+        <translation>翻譯習慣用語</translation>
+    </message>
+    <message>
+        <source>Enables translation of Mainland Chinese idioms into Taiwanese and vice versa.</source>
+        <translation>開啓中國大陸與台灣地區之間習慣用語相互轉換</translation>
+    </message>
+    <message>
+        <source>From          ​​</source>
+        <translation>从          ​​</translation>
+    </message>
+    <message>
+        <source>To          </source>
+        <translation>到          ​​</translation>
+    </message>
+    <message>
+        <source>Enter Chinese text to convert...</source>
+        <translation>輸入中文文本以轉換…</translation>
+    </message>
+    <message>
+        <source>Source text</source>
+        <translation>源文本</translation>
+    </message>
+    <message>
+        <source>Conversion results</source>
+        <translation>轉換結果</translation>
+    </message>
+</context>
+</TS>
