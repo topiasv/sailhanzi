5147c50c455721725f6e95db6fb0ccd28b748738
dashinfantry <45334556+dashinfantry@users.noreply.github.com>
2019-04-20 20:33:13 +0800

    Create harbour-sailhanzi-zh_cn.ts

---
+++ translations/harbour-sailhanzi-zh_cn.ts
@@ -0,0 +1,66 @@
+<?xml version="1.0" encoding="utf-8"?>
+<!DOCTYPE TS>
+<TS version="2.1">
+<context>
+    <name>CoverPage</name>
+    <message>
+        <source>SailHanzi</source>
+        <translation>航海汉字</translation>
+    </message>
+</context>
+<context>
+    <name>MainPage</name>
+    <message>
+        <source>Convert</source>
+        <translation>转换</translation>
+    </message>
+    <message>
+        <source>SailHanzi</source>
+        <translation>航海汉字</translation>
+    </message>
+    <message>
+        <source>Simplified</source>
+        <translation>简体汉字</translation>
+    </message>
+    <message>
+        <source>Traditional</source>
+        <translation>传统汉字</translation>
+    </message>
+    <message>
+        <source>Taiwanese</source>
+        <translation>中国台湾</translation>
+    </message>
+    <message>
+        <source>Hong Kong</source>
+        <translation>中国香港</translation>
+    </message>
+    <message>
+        <source>Translate idioms</source>
+        <translation>翻译习惯用语</translation>
+    </message>
+    <message>
+        <source>Enables translation of Mainland Chinese idioms into Taiwanese and vice versa.</source>
+        <translation>开启中国大陆与台湾地区习惯用语相互转换</translation>
+    </message>
+    <message>
+        <source>From          ​​</source>
+        <translation>从          ​​</translation>
+    </message>
+    <message>
+        <source>To          </source>
+        <translation>到</translation>
+    </message>
+    <message>
+        <source>Enter Chinese text to convert...</source>
+        <translation>输入汉语文本以转换……</translation>
+    </message>
+    <message>
+        <source>Source text</source>
+        <translation>源文本</translation>
+    </message>
+    <message>
+        <source>Conversion results</source>
+        <translation>转换结果</translation>
+    </message>
+</context>
+</TS>
