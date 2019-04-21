a1af70c1b269d310aaa41b5b80288c23d6a3a9b3
dashinfantry <45334556+dashinfantry@users.noreply.github.com>
2019-04-21 21:46:59 +0800

    Create harbour-sailhanzi-zh_CN.ts

---
+++ harbour-sailhanzi-zh_CN.ts
@@ -0,0 +1,66 @@
+<?xml version="1.0" encoding="utf-8"?>
+<!DOCTYPE TS>
+<TS version="2.1">
+<context>
+    <name>CoverPage</name>
+    <message>
+        <source>SailHanzi</source>
+        <translation>风帆汉字</translation>
+    </message>
+</context>
+<context>
+    <name>MainPage</name>
+    <message>
+        <source>Convert</source>
+        <translation>转换</translation>
+    </message>
+    <message>
+        <source>SailHanzi</source>
+        <translation>风帆汉字</translation>
+    </message>
+    <message>
+        <source>Simplified</source>
+        <translation>简体字</translation>
+    </message>
+    <message>
+        <source>Traditional</source>
+        <translation>繁体字</translation>
+    </message>
+    <message>
+        <source>From          </source>
+        <translation>从</translation>
+    </message>
+    <message>
+        <source>To          </source>
+        <translation>到</translation>
+    </message>
+    <message>
+        <source>Enter Chinese text to convert...</source>
+        <translation>输入汉语文本以转换</translation>
+    </message>
+    <message>
+        <source>Source text</source>
+        <translation>源文本</translation>
+    </message>
+    <message>
+        <source>Conversion results</source>
+        <translation>转换结果</translation>
+    </message>
+    <message>
+        <source>Taiwan Traditional</source>
+        <translation>台湾繁体</translation>
+    </message>
+    <message>
+        <source>Hong Kong Traditional</source>
+        <translation>香港繁体</translation>
+    </message>
+    <message>
+        <source>Translate regional vocabulary</source>
+        <translation>翻译地区性词汇</translation>
+    </message>
+    <message>
+        <source>Enables translation of commonly used vocabulary between Mainland Chinese and Taiwanese.</source>
+        <translation>开启中国大陆与台湾地区惯用词汇翻译</translation>
+    </message>
+</context>
+</TS>
