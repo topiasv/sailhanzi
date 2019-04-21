29ee8dd64ab651e3e7a030c7b962e593d25f1aa0
dashinfantry <45334556+dashinfantry@users.noreply.github.com>
2019-04-21 21:41:16 +0800

    Create harbour-sailhanzi-zh_HK.ts

---
+++ translations/harbour-sailhanzi-zh_HK.ts
@@ -0,0 +1,66 @@
+?xml version="1.0" encoding="utf-8"?>
+<!DOCTYPE TS>
+<TS version="2.1">
+<context>
+    <name>CoverPage</name>
+    <message>
+        <source>SailHanzi<source>
+        <translation>風帆漢字</translation>
+    </message>
+</context>
+<context>
+    <name>MainPage</name>
+    <message>
+        <source>Convert</source>
+        <translation>轉換</translation>
+    </message>
+    <message>
+        <source>SailHanzi</source>
+        <translation>風帆漢字</translation>
+    </message>
+    <message>
+        <source>Simplified</source>
+        <translation>簡體漢字</translation>
+    </message>
+    <message>
+        <source>Traditional</source>
+        <translation>繁體字</translation>
+    </message>
+    <message>
+        <source>From          </source>
+        <translation>從</translation>
+    </message>
+    <message>
+        <source>To          </source>
+        <translation>到</translation>
+    </message>
+    <message>
+        <source>Enter Chinese text to convert...</source>
+        <translation>輸入漢語文本以轉換</translation>
+    </message>
+    <message>
+        <source>Source text</source>
+        <translation>源文本</translation>
+    </message>
+    <message>
+        <source>Conversion results</source>
+        <translation>轉換結果</translation>
+    </message>
+    <message>
+        <source>Taiwan Traditional</source>
+        <translation>臺灣繁體</translation>
+    </message>
+    <message>
+        <source>Hong Kong Traditional</source>
+        <translation>香港繁體</translation>
+    </message>
+    <message>
+        <source>Translate regional vocabulary</source>
+        <translation>翻譯區域慣用語彙</translation>
+    </message>
+    <message>
+        <source>Enables translation of commonly used vocabulary between Mainland Chinese and Taiwanese.</source>
+        <translation>開啓中國大陸與臺灣地區慣用語彙翻譯</translation>
+    </message>
+</context>
+</TS>
